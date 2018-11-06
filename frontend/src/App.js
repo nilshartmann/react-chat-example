d5b8aef8110fb3516fa1835f69c99b4a92d722fe
Nils Hartmann <nils@nilshartmann.net>
2018-11-06 11:24:03 +0100

    fix navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ export default class App extends React.Component {
           <Layout>
             {visiblePage === "chat" && <ChatPage onExitChat={this.onExitChat} openDashboard={this.onOpenDashboard} />}
             {visiblePage === "thankyou" && <ThankYou reconnect={this.onOpenChat} />}
-            {visiblePage === "dashboard" && <DashboardPage reconnect={this.onOpenChat} />}
+            {visiblePage === "dashboard" && <DashboardPage reconnect={this.onOpenChat} onClose={this.onOpenChat} />}
           </Layout>
         </ChatProvider>
       </React.Suspense>
